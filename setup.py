859253cf2264450a26ef502a7aeda61df7e9f1a8
Day Tripper <kekovdom@gmail.com>
2024-04-09 11:20:18 +0300

    Удалено версионирование в setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=[
-        "expression>=5.0.2",
-        "more-itertools>=10.2.0",
-        "pillow>=10.3.0",
-        "pytest>=8.1.1",
+        "expression",
+        "more-itertools",
+        "pillow",
+        "pytest",
     ],
 )
